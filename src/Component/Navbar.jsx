02ec896499a5b120c61c7e17fd1babead1412f00
sheri418 <sherazgoraya418@gmail.com>
2024-07-30 03:39:27 +0500

    navbar complete

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import logo from "../Assets/Logo.svg";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+// import { Link } from 'react-router-dom';
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCart";
+
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Contact",
+      icon: <PhoneRoundedIcon />,
+    },
+    {
+      text: "Blog",
+      icon: <CommentRoundedIcon />,
+    },
+    {
+      text: "Cart",
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
+
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={logo} alt="Logo" className="nav-logo" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        <a href="/blog">Blog</a>
+        <a href="/cart">
+          <BsCart2 className="navbar-cart-icon" />
+        </a>
+        <button className="primary-button">Booking Now</button>
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="left">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {item.icon}
+                    <ListItemText primary={item.text} />
+                  </ListItemIcon>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </nav>
+  );
+};
+
+export default Navbar;
